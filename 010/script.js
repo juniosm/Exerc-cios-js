e3197ffabfaa19fb1aeb78e246854c157c7e5d0a
Junio Sousa <juniosousa120@gmail.com>
2022-10-28 02:28:42 -0300

    First execícios

---
+++ 010/script.js
@@ -0,0 +1,16 @@
+const resul = document.getElementById("resultado");
+
+function acao1() {
+  resul.innerHTML += " <p>ação 1</p>";
+}
+
+function acao2() {
+  resul.innerHTML += " <p>ação 2</p>";
+}
+
+function acao3() {
+  resul.innerHTML += " <p>ação 3</p>";
+}
+function acao4() {
+  resul.innerHTML += " <p>ação 4</p>";
+}
